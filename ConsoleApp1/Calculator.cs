0a7f79f29aba4bdcd0deda207362b9dbc773fa2a
Akito0310 <bachubucha@gmail.com>
2025-01-29 20:03:35 +0700

    Initial commit with code and tests

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static decimal Divide(decimal a, decimal b)
+        {
+            if (b == 0) throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
+    }
+}
---
+++ Test_ConsoleApp1/Test1.cs
@@ -0,0 +1,48 @@
+﻿namespace Test_ConsoleApp1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Test_Add()
+        {
+            // Arrange
+            decimal a = 2;
+            decimal b = 3;
+
+            // Act
+            decimal result = Calculator.Add(a, b);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Test_Divide()
+        {
+            // Arrange
+            decimal a = 6;
+            decimal b = 3;
+
+            // Act
+            decimal result = Calculator.Divide(a, b);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Test_DivideByZero()
+        {
+            // Arrange
+            decimal a = 5;
+            decimal b = 0;
+
+            // Act
+            Calculator.Divide(a, b);
+
+            // Assert (handled by ExpectedException)
+        }
+    }
+}
